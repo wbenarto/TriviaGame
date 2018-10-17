fc5da3a0c7e6288dec8ac8ff6d40d5dba8ace13c
w.benarto <w.benarto@gmail.com>
2018-10-16 18:25:24 -0700

    pseudocode javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//Set global variables
+
+
+//Function to generate countdown timer 
+//Function to generate random questions
+//Function to generate answer list
+//Function to start game
+    //Game starts, countdown starts
+    //Generate questions and display in page
+    //Generate list of possible answers and display in page
+    
+//set up rules for the game
+    //game starts and when user click on the answer
+        //show right or wrong 
+        //adds score 
+        //show the right answer
+        //if timer runs out and no clicks, increment wrong answer
+        //go to another question
